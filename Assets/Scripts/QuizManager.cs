e16938aac9af520f5ce96afa0451c70fbd9cd174
EyaBk1402 <161072492+EyaBk1402@users.noreply.github.com>
2025-06-11 00:44:09 +0200

    Mise à jour : modifications récentes du projet

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -7,22 +7,18 @@ using System.Collections.Generic;
 public class QuizManager : MonoBehaviour
 {
     public static QuizManager Instance;
-
-    [Header("UI References")]
     public Image[] imageSlots;
     public Button[] answerButtons;
+    public Button nextButton;
+    public Button repeatButton;
     public ProgressController progressController;
     public TextMeshProUGUI timerText;
     public TextMeshProUGUI topbarCoinsText;
-
-    [Header("Sprites & Rounds")]
     public Sprite[] allSprites;
     public GameObject popupWin;
     public GameObject popupLose;
     public Image[] starIcons;
     public TextMeshProUGUI coinsText;
-    public Button nextButton;
-    public Button repeatButton;
 
     private int[] roundIndices;
     private Dictionary<Button, int> mapping;
@@ -35,24 +31,20 @@ public class QuizManager : MonoBehaviour
     private float timeRemaining;
     private bool timerRunning;
 
-    private const int totalRounds = 10;
+    private const int totalRounds = 21;
     private readonly int[] coinsPerStar = { 10, 30, 60, 100 };
 
-    void Awake()
-    {
-        Instance = this;
-    }
+    void Awake() { Instance = this; }
 
     void Start()
     {
-        
         int init;
         var parts = topbarCoinsText.text.Split(' ');
         totalCoins = int.TryParse(parts[0], out init) ? init : 0;
-        topbarCoinsText.text = totalCoins + "";
-
+        topbarCoinsText.text = totalCoins + " ";
         nextButton.onClick.AddListener(OnNextRound);
         repeatButton.onClick.AddListener(OnRepeatRound);
+        currentRound = 0;
         SetupRound();
     }
 
@@ -65,6 +57,7 @@ public class QuizManager : MonoBehaviour
         {
             timerRunning = false;
             popupLose.SetActive(true);
+            repeatButton.gameObject.SetActive(true);
         }
     }
 
@@ -75,45 +68,32 @@ public class QuizManager : MonoBehaviour
 
     void SetupRound()
     {
-        currentRound = currentRound < totalRounds ? currentRound + 1 : 1;
-        InitRound();
-
-        roundIndices = Enumerable.Range(0, allSprites.Length)
-                                 .OrderBy(i => Random.value)
-                                 .Take(3)
-                                 .ToArray();
-
+        if (currentRound < totalRounds) currentRound++;
+        attempts = 0;
+        matched = new bool[3];
+        matchedCount = 0;
+        selectedSlot = -1;
+        popupWin.SetActive(false);
+        popupLose.SetActive(false);
+        nextButton.gameObject.SetActive(false);
+        repeatButton.gameObject.SetActive(false);
+        timeRemaining = 60f;
+        timerRunning = true;
+        timerText.text = "60";
+        roundIndices = Enumerable.Range(0, allSprites.Length).OrderBy(i => Random.value).Take(3).ToArray();
         mapping = new Dictionary<Button, int>();
-        var options = roundIndices
-                      .Select(i => new { n = allSprites[i].name, idx = i })
-                      .OrderBy(x => Random.value)
-                      .ToArray();
-
+        var options = roundIndices.Select(i => new { n = allSprites[i].name, idx = i }).OrderBy(x => Random.value).ToArray();
         for (int i = 0; i < 3; i++)
         {
             imageSlots[i].sprite = allSprites[roundIndices[i]];
             var click = imageSlots[i].GetComponent<ClickableImage>();
             if (click != null) click.slotIndex = i;
-
             var btn = answerButtons[i];
             btn.onClick.RemoveAllListeners();
             mapping[btn] = options[i].idx;
             btn.GetComponentInChildren<TextMeshProUGUI>().text = options[i].n;
             btn.onClick.AddListener(() => OnAnswerSelected(btn));
         }
-    }
-
-    void InitRound()
-    {
-        attempts = 0;
-        matched = new bool[3];
-        matchedCount = 0;
-        popupWin.SetActive(false);
-        popupLose.SetActive(false);
-        timeRemaining = 60f;
-        timerRunning = true;
-        timerText.text = "60";
-        selectedSlot = -1;
     }
 
     public void OnAnswerSelected(Button btn)
@@ -128,15 +108,16 @@ public class QuizManager : MonoBehaviour
             {
                 timerRunning = false;
                 popupWin.SetActive(true);
-                int stars = attempts == 0 ? 3 :
-                            attempts <= 3 ? 2 :
-                            attempts <= 5 ? 1 : 0;
+                int stars = attempts == 0 ? 3 : attempts <= 3 ? 2 : attempts <= 5 ? 1 : 0;
                 for (int i = 0; i < starIcons.Length; i++)
                     starIcons[i].color = i < stars ? Color.yellow : Color.gray;
-                coinsText.text = "+" + coinsPerStar[stars];
-                totalCoins += coinsPerStar[stars];
-                topbarCoinsText.text = totalCoins + " coins";
+                int earned = coinsPerStar[stars];
+                totalCoins += earned;
+                coinsText.text = "+" + earned;
+                topbarCoinsText.text = totalCoins + " ";
                 progressController.AdvanceRound();
+                nextButton.gameObject.SetActive(currentRound < totalRounds);
+                repeatButton.gameObject.SetActive(stars < 3);
             }
         }
         else
@@ -146,6 +127,7 @@ public class QuizManager : MonoBehaviour
             {
                 timerRunning = false;
                 popupLose.SetActive(true);
+                repeatButton.gameObject.SetActive(true);
             }
         }
 
@@ -156,10 +138,22 @@ public class QuizManager : MonoBehaviour
     {
         if (currentRound < totalRounds)
             SetupRound();
+        else
+            nextButton.gameObject.SetActive(false);
     }
 
     public void OnRepeatRound()
     {
-        InitRound();
+        attempts = 0;
+        matched = new bool[3];
+        matchedCount = 0;
+        selectedSlot = -1;
+        popupWin.SetActive(false);
+        popupLose.SetActive(false);
+        timerRunning = true;
+        timeRemaining = 60f;
+        timerText.text = "60";
+        nextButton.gameObject.SetActive(false);
+        repeatButton.gameObject.SetActive(false);
     }
 }
